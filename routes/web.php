5a26eafd8e2bd23ca59972127b0cf8a0213f192a
wmhas <whashim@gmail.com>
2021-05-26 21:37:05 +0800

    'update'

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,25 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Support\LazyCollection;
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/lazy', function () {
+
+    $collection = LazyCollection::times( 20)
+        ->map(
+            function($number){ return $number*3;}
+        )
+    ->all();
+
+    dump($collection);
+
+    return "done";
+});
+
+
+
